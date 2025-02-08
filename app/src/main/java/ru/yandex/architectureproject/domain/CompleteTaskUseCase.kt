9285f13de2d171abfa73802bdfe77699dafc3b98
vasiliy <vaszol@mail.ru>
2025-02-08 09:21:14 +0300

    Проведение рефакторинга

--- app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
+++ app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
@@ -1,12 +1,16 @@
 package ru.yandex.architectureproject.domain
 
+import kotlinx.coroutines.delay
 import ru.yandex.architectureproject.data.repository.TaskRepository
+
+private const val TASK_DELETION_DELAY_MILLIS = 10_000L
 
 class CompleteTaskUseCase(
     private val repository: TaskRepository,
 ) {
     suspend operator fun invoke(taskId: Int) {
         repository.completeTask(taskId)
-        // TODO: Здесь будет автоудаление задачи
+        delay(TASK_DELETION_DELAY_MILLIS)
+        repository.deleteTask(taskId)
     }
 }
